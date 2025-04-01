4fe20ee5c05ee263ee4d5d19df105abf9c84b588
samuel <adeyemis710@gmail.com>
2025-04-01 20:17:09 +0100

    fixes

--- pages/dinner-pics/index.tsx
+++ pages/dinner-pics/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLastViewedPhoto } from "../../utils/useLastViewedPhoto";
 import Modal from "../../components/Modal";
 import Hero from "../../components/Hero";
@@ -12,12 +12,14 @@ import cloudinary from "../../utils/cloudinary";
 import getBase64ImageUrl from "../../utils/generateBlurPlaceholder";
 import { ImageProps } from "../../utils/types";
 import HeroComp from "../../components/HeroComp";
+import { ArrowDownTrayIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import downloadPhoto from "../../utils/downloadPhoto";
 
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   const router = useRouter();
   const { photoId } = router.query;
   const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
-
+  const [selectedImage, setSelectedImage] = useState<ImageProps | null>(null);
   const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
 
   useEffect(() => {
@@ -51,34 +53,74 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         <HeroComp
          text='My Dinner Photography'
         />
-        <div className="columns-1 p-4 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          {images.map(({ id, public_id, format, blurDataUrl }) => (
-            <Link
-              key={id}
-              href={`/?photoId=${id}`}
-              as={`/p/${id}`}
-              ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-              shallow
-              className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-            >
-              <Image
-                alt="olalifestyle photography"
-                className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                style={{ transform: "translate3d(0, 0, 0)" }}
-                placeholder="blur"
-                blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-                width={720}
-                height={480}
-                sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw"
-              />
-            </Link>
-          ))}
-        </div>
-      </main>
+          <div className="columns-1 p-4 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+    {images.map(({ id, public_id, format, blurDataUrl }) => (
+      <div
+        onClick={() => setSelectedImage({ id, public_id, format, blurDataUrl })}
+        key={id}
+        ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+        className="group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+      >
+        <Image
+          alt="olalifestyle photography"
+          className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+          placeholder="blur"
+          blurDataURL={blurDataUrl}
+          src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+          width={720}
+          height={480}
+          sizes="(max-width: 640px) 100vw,
+                (max-width: 1280px) 50vw,
+                (max-width: 1536px) 33vw,
+                25vw"
+        />
+      </div>
+    ))}
+  </div>
+
+  {/* MODAL OUTSIDE THE MAP */}
+  {selectedImage && (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black z-50">
+        
+        <div className="relative">
+         <div 
+          className="absolute top-0 p-5 w-full flex items-center justify-between  rounded-full">
+        <button
+        onClick={() => setSelectedImage(null)}
+        className="rounded-full  bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+        >
+          <XMarkIcon className="w-6 h-6" />
+        </button>
+        
+         <button
+          onClick={() =>
+            downloadPhoto(
+              `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${selectedImage.public_id}.${selectedImage.format}`,
+              `${selectedImage.id}.jpg`
+            )
+          }
+          className="rounded-full  bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+          title="Download fullsize version"
+        >
+          <ArrowDownTrayIcon className="h-5 w-5" />
+        </button>
+
+          </div>
+
+        <Image
+          src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_1080/${selectedImage.public_id}.${selectedImage.format}`}
+          width={1000}
+          height={600}
+          className='h-screen object-cover'
+          loading="lazy"
+          alt="blurred background"
+          blurDataURL={selectedImage.blurDataUrl}
+        />
+       
+      </div>
+    </div>
+  )}
+</main>
       <Footer />
     </>
   );
